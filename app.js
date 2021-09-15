d8c0c62728e1de445a35ca4e3cae148bdf2957a5
Ajat Darojat <ajatdarojat45@gmail.com>
2021-09-16 06:23:55 +0700

    add login feature

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.post("/login", async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		const user = await User.findOne({ email, password });
+		res.status(200).json({ user });
+	} catch (error) {
+		console.log("🚀 ~ file: app.js ~ line 9 ~ app.post ~ error", error);
+	}
+});
+
 app.listen(PORT, () => {
 	console.log("🚀 ~ file: app.js ~ line 6 ~ app.listen ~ PORT", PORT);
 });
